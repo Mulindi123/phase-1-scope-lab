8a0af7cf75b021c0e55211fc37efd962c4b05a9d
Medrine Musabi Mulindi <medrine.mulindi@student.moringaschool.com>
2023-07-13 06:29:21 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var customerName = 'bob';
+ function  upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    return customerName;
+  }
+
+ // console.log(upperCaseCustomerName('bob'));
+
+
+
+var setBestCustomer = function () {
+  bestCustomer = 'not bob';
+}
+
+var overwriteBestCustomer = function () {
+  bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'initial value';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'new value'; 
+}
